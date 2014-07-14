5b2fc89ad4452962c952645c67e71e76df969472
Daniel Berry <danielberrytn@gmail.com>
2014-07-14 08:50:40 -0500

    adds our partials
    Signed-off-by: Daniel Berry <danielberrytn@gmail.com>

---
+++ app/views/layouts/partials/footer.blade.php
@@ -0,0 +1,7 @@
+<!-- jQuery -->
+<script src="//code.jquery.com/jquery.js"></script>
+<!-- Bootstrap JavaScript -->
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
+
+@section('scripts')
+@show
