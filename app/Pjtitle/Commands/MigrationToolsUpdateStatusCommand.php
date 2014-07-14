df92e22604899cc9d00e25b7b80a1c550c1aff40
Daniel Berry <danielberrytn@gmail.com>
2014-07-14 01:07:53 -0500

    adds status update task.
    Signed-off-by: Daniel Berry <danielberrytn@gmail.com>

---
+++ app/Pjtitle/Commands/MigrationToolsUpdateStatusCommand.php
@@ -0,0 +1,104 @@
+<?php namespace Pjtitle\Commands;
+
+use App;
+use Carbon\Carbon;
+use File;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Mail;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\HttpFoundation\FileBag;
+
+class MigrationToolsUpdateStatusCommand extends Command {
+
+	/**
+	 * The console command name.
+	 *
+	 * @var string
+	 */
+	protected $name = 'migration_tools:status_update_utility';
+
+	/**
+	 * The console command description.
+	 *
+	 * @var string
+	 */
+	protected $description = 'Updates the legacy prime migration status.';
+
+	/**
+	 * Create a new command instance.
+	 *
+	 * @return void
+	 */
+	public function __construct()
+	{
+		parent::__construct();
+
+        $this->prime = App::make('\PjtitleData\VcConnector\Repositories\PrimeRepository');
+	}
+
+	/**
+	 * Execute the console command.
+	 *
+	 * @return mixed
+	 */
+	public function fire()
+	{
+        $primes = $this->prime->createModel()
+            ->has('migrationStatus', '=', 0)
+            ->take(100)
+            ->with('priorReferences')
+            ->with('migrationStatus')
+            ->get();
+
+        $totalPrimes = count($primes);
+
+        if ($totalPrimes <= 0)
+        {
+            exit;
+        }
+
+        foreach ($primes as $prime)
+        {
+            $statusUpdate = [
+                'has_been_migrated'        => 0,
+                'missing_prior_references' => 0
+            ];
+
+            $apiInput = ['oldCwlId' => $prime->old_cwl_id];
+
+            $results = $service = (new \PjtitleData\SearchApi\Api\Services\AlfrescoSearchService($apiInput))->getResults();
+
+            if ( count($results) >= 2 )
+            {
+                $statusUpdate['has_duplicates'] = 1;
+            }
+
+            if ( ! empty($results[0]) )
+            {
+                $statusUpdate['has_been_migrated'] = 1;
+                $results = $results[0];
+
+                $statusUpdate['date_migrated'] = (new \Carbon\Carbon("2013-10-15T23:14:51+0000"))->toDateTimeString();
+
+                $shouldHavePriorReferences = ! empty($prime->getPriorReferencesArray());
+                $migratedHasPriorReferences = ! empty($results['cwlvcda:priorReference']);
+
+                if ( $shouldHavePriorReferences == true and $migratedHasPriorReferences == false )
+                {
+                    $statusUpdate['missing_prior_references'] = 0;
+                }
+            }
+
+            $status = new \PjtitleData\VcConnector\Models\MigrationStatus($statusUpdate);
+
+            $prime->migrationStatus()->save($status);
+        }
+
+        $finishTime = Carbon::now()->toDateTimeString();
+        $filePath = base_path() . "/logs/statusUpdateCron.txt";
+
+        File::append($filePath, "\n{$totalPrimes} were updated at {$finishTime}");
+
+	}
+}
--- app/start/artisan.php
+++ app/start/artisan.php
@@ -11,3 +11,4 @@
 |
 */
 
+Artisan::add(new \Pjtitle\Commands\MigrationToolsUpdateStatusCommand);
