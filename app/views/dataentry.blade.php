5cd5956dcb57ee5145837d7b932f6284d7d00d8b
Daniel Berry <danielberrytn@gmail.com>
2014-07-15 16:52:36 -0500

    updates grid demo views
    Signed-off-by: Daniel Berry <danielberrytn@gmail.com>

--- app/views/dataentry.blade.php
+++ app/views/dataentry.blade.php
@@ -3,16 +3,17 @@
 @section('styles')
 @parent
 
-{{ HTML::style('css/handsOnTable.full.css') }}
-{{ HTML::style('css/handsOnTable.bootstrap.css') }}
+{{ HTML::style('css/table.minified.css') }}
 @stop
 
 @section('scripts')
 @parent
-{{ HTML::script('js/handsOnTable.full.js') }}
+{{ HTML::script('js/table.minified.js') }}
 
 <script type="text/javascript">
     $(function() {
+
+        var ht;
 
         var maxed = false
             , resizeTimeout
@@ -30,8 +31,6 @@
             }
         };
 
-        var ip_validator_regexp = /^(?:\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b|null)$/;
-
         var data = [
             [, '', , , 'COUNTRY TRAILS SUBDIVISION UNIT 7', 4, ''],
             [507, 'NAVARRO, N', 326, 32, 'Live Bee Land Subdivision No 4', 116, 14]
@@ -39,13 +38,12 @@
 
         $('#example1').handsontable({
             data: data,
-            colHeaders: ["Abstract #", "Survey Name", "Survey #", "Acreage", "Subdivision", "Block", "Lot"],
+            colHeaders: ["Abstract #", "Survey Name", "Survey #", "Ac", "Subdivision", "Block", "Lot"],
             minSpareRows: 1,
-            colWidths: [100, 300, 85, 80, 300, 80, 80],
+            colWidths: [80, 250, 70, 40, 300, 60, 60],
             contextMenu: true,
             manualColumnMove: true,
-            manualColumnResize: true,
-            persistentState: true
+            manualColumnResize: true
         });
 
         $('#example1 table').addClass('table');
@@ -54,12 +52,85 @@
             var method = this.checked ? 'addClass' : 'removeClass';
             $('#example1').find('table')[method](this.getAttribute('data-type'));
         });
+
+        var childWindow;
+
+        $("#loader").on('click', function() {
+            openChildWindow();
+        });
+
+        $("#closer").on('click', function() {
+            closeChildWindow();
+        });
+
+        $("#google").on('click', function() {
+            refreshChildWindow();
+        });
+
+        function openChildWindow() {
+            if (childWindow) {
+                alert("We already have one open.");
+            }
+            else {
+                childWindow = window.open('preview-window', '_blank', 'location=yes,toolbar=no');
+            }
+        }
+
+        function closeChildWindow() {
+            if (!childWindow) {
+                alert("There is no child window open.");
+            }
+            else {
+                childWindow.close();
+                childWindow = undefined;
+            }
+        }
+
+        function refreshChildWindow() {
+            if ( ! childWindow) {
+                alert("There is no child window open.");
+            }
+            else {
+                childWindow.location = 'http://google.com';
+            }
+        }
     });
 </script>
 @stop
 
 @section('content')
 
-<div id="example1"></div>
+<div class="well">
+    <p>
+        This is a really quick demo of two features that Kristin has talked about both of these
+        on several occasions.
+    </p>
+
+    <p>
+        The table acts like excel, drag columns, use arrow keys to change rows, can copy multiple rows, etc.. Validation
+        and autocomplete should still be available as well as auto-formatting of cells.
+    </p>
+
+    <p>
+        The preview with the ability to be opened in a seperate window that would allow dual monitor use.  Ideally, when
+        the previewer is opened, that original preview div would hide, the data entry form would expand to 100% coverage
+        giving plenty of room. If the previewer was closed by a button in the child window, the preview div would display
+        again in the normal spot and the data entry form would collapse back to normal size. <em>Those features are not in here, it's just a demo and didn't have time to fully dev it out.</em>
+    </p>
+</div>
+
+<div class="row">
+    <div class="col-sm-6" id="grid">
+        <div id="example1"></div>
+    </div>
+
+    <div class="col-sm-6" id="previewer">
+
+        <button id="loader" class="btn btn-default btn-xs">Open in New Window</button>
+        <button id="closer" class="btn btn-default btn-xs">Close Window</button>
+        <p></p>
+        <embed src="/images/00107201.PDF" width="100%" height="700">
+    </div>
+</div>
 
 @stop
--- app/views/layouts/default-fluid.blade.php
+++ app/views/layouts/default-fluid.blade.php
@@ -19,17 +19,8 @@
 	<body>
 
         <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
-            <div class="container">
-                <a class="navbar-brand" href="#">Migration Status Tool</a>
-                <ul class="nav navbar-nav">
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Status Utility <span class="caret"></span></a>
-                        <ul class="dropdown-menu" role="menu">
-                            <li><a href="{{ URL::to('tools/status-update-utility') }}">Run Utility</a></li>
-                            <li><a href="{{ URL::to('tools/status-update-utility/logs') }}">Logs</a></li>
-                        </ul>
-                    </li>
-                </ul>
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">Data Entry Samples</a>
             </div>
         </nav>
 
